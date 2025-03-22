f0fe6345ea06d326639914f48935c2035b304bcb
BhavayaGoyal <bhavayagoyal89@gmail.com>
2025-03-22 10:34:39 +0530

    Update app1.js

--- softech2/app1.js
+++ softech2/app1.js
@@ -2,9 +2,10 @@ const express = require('express')
 const cors= require('cors')
 const app = express();
 const indexRouter=require('./Router/index')
-app.use(express.json())
-app.use('/',indexRouter)
 app.use(cors())
+app.use(express.json())
+
+app.use('/',indexRouter)
 app.listen(3000, () => {
     console.log("server started on port 3000")
 });
